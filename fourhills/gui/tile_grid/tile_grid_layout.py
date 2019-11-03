7edf8549fd7664735ad1c7e946fdcfcb52acc3d6
Michael Hart <michael.hart1994@gmail.com>
2019-11-03 14:00:21 +0000

    Corrected forcing the grid to be the correct size

--- fourhills/gui/tile_grid/tile_grid_layout.py
+++ fourhills/gui/tile_grid/tile_grid_layout.py
@@ -13,21 +13,31 @@ class TileGridLayout(QtWidgets.QGridLayout):
         self.setContentsMargins(0, 0, 0, 0)
         self.rows = rows
         self.cols = cols
-        self.fill_bottom_right()
+        self.fill_diagonal()
 
         # Make sure cell items fill their cells
         self.setSpacing(0)
         self.setContentsMargins(0, 0, 0, 0)
 
-    def fill_bottom_right(self):
-        self.bottom_right_cell = QtWidgets.QWidget()
-        super().addWidget(self.bottom_right_cell, self.rows - 1, self.cols - 1)
+    def fill_diagonal(self):
+        # Put generic widgets in diagonals, or if row is bigger than cols
+        # or vice versa, fill the end row/column
+        for idx in range(max(self.rows, self.cols)):
+            row = min(idx, self.rows)
+            col = min(idx, self.cols)
+            item = self.itemAtPosition(row, col)
+            if item is None:
+                super().addWidget(QtWidgets.QWidget(), row, col)
+
+    # def fill_bottom_right(self):
+    #     self.bottom_right_cell = QtWidgets.QWidget()
+    #     super().addWidget(self.bottom_right_cell, self.rows - 1, self.cols - 1)
 
     def set_grid_size(self, rows, cols):
         self.removeWidget(self.bottom_right_cell)
         self.rows = rows
         self.cols = cols
-        self.fill_bottom_right()
+        self.fill_diagonal()
 
     def addWidget(
             self,
@@ -70,4 +80,4 @@ class TileGridLayout(QtWidgets.QGridLayout):
             super().removeWidget(correct_widget)
             self.widgets.remove(correct_wrapper)
             correct_widget.hide()
-            self.fill_bottom_right()
+            self.fill_diagonal()
